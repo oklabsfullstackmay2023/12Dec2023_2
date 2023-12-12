2cce3fd0ddda7b734b8a42b96603d8de0a8bc96f
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-12 17:22:48 +0530

    hello

---
+++ friends.py
@@ -0,0 +1,14 @@
+bestfriends = []
+
+print(f"Please enter your 4 best friends is ")
+      
+for i in range(4):
+    bestfriend = input(f"friends {i+1} ")
+    bestfriends.append(bestfriend)
+
+print(bestfriends)
+
+for bestfriend in bestfriends:
+    print(f"My best friends is {bestfriend} ")
+
+
